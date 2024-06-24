398e108bd9ae34ec64a1c86af3e8a2f40a1faeab
FajarHamonangan <bengkelws@gmail.com>
2024-06-24 20:30:22 +0700

    setting aplikasi & route awal

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('hello world')
+const exp = require('constants')
+const express   = require('express')
+const app       = express()
+const port      = 3000
+const c_beranda = require('./controller/c_beranda')
+
+app.use( express.urlencoded({extended:false}) )
+app.set('view engine', 'ejs')
+app.set('views','./view-html')
+
+
+app.get('/', c_beranda.index)
+
+
+app.listen(port, ()=>{
+    console.log(`Aplikasi sudah siap, buka http://localhost:${port}`)
+})
